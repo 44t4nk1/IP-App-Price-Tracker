5a76db590db12d69fbd63c9c9e379e69de8503bb
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2020-12-05 11:43:28 +0530

    Initial Screen Done

--- pricetracker/lib/screens/initialScreen.dart
+++ pricetracker/lib/screens/initialScreen.dart
@@ -46,7 +46,7 @@ class _InitialScreenState extends State<InitialScreen> {
             ),
           ),
           SizedBox(
-            height: size.height * 2 / 100,
+            height: size.height * 3 / 100,
           ),
           Row(
             children: [
@@ -56,7 +56,7 @@ class _InitialScreenState extends State<InitialScreen> {
                 child: Text(
                   "Food",
                   style: GoogleFonts.montserrat(
-                    fontSize: 24,
+                    fontSize: 20,
                     fontWeight: FontWeight.w700,
                     textStyle: Theme.of(context).textTheme.bodyText1,
                   ),
@@ -69,16 +69,13 @@ class _InitialScreenState extends State<InitialScreen> {
                 child: Text(
                   "₹ " + _foodValue.round().toString(),
                   style: GoogleFonts.montserrat(
-                    fontSize: 24,
+                    fontSize: 20,
                     fontWeight: FontWeight.w700,
                     textStyle: Theme.of(context).textTheme.bodyText1,
                   ),
                 ),
               ),
             ],
-          ),
-          SizedBox(
-            height: size.height * 2 / 100,
           ),
           Container(
             width: double.infinity,
@@ -96,6 +93,233 @@ class _InitialScreenState extends State<InitialScreen> {
               divisions: 100,
               min: 0,
               max: 20000,
+            ),
+          ),
+          SizedBox(
+            height: size.height * 1 / 100,
+          ),
+          Row(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(left: size.width * 5 / 100),
+                child: Text(
+                  "Clothes",
+                  style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    textStyle: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+              ),
+              Spacer(),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(right: size.width * 5 / 100),
+                child: Text(
+                  "₹ " + _clothesValue.round().toString(),
+                  style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    textStyle: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: size.width * 5 / 100, right: size.width * 5 / 100),
+            child: Slider(
+              activeColor: Theme.of(context).accentColor,
+              inactiveColor: Theme.of(context).backgroundColor,
+              value: _clothesValue,
+              onChanged: (newValue) {
+                setState(() {
+                  _clothesValue = newValue;
+                });
+              },
+              divisions: 100,
+              min: 0,
+              max: 20000,
+            ),
+          ),
+          SizedBox(
+            height: size.height * 1 / 100,
+          ),
+          Row(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(left: size.width * 5 / 100),
+                child: Text(
+                  "Fuel",
+                  style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    textStyle: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+              ),
+              Spacer(),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(right: size.width * 5 / 100),
+                child: Text(
+                  "₹ " + _fuelValue.round().toString(),
+                  style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    textStyle: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: size.width * 5 / 100, right: size.width * 5 / 100),
+            child: Slider(
+              activeColor: Theme.of(context).accentColor,
+              inactiveColor: Theme.of(context).backgroundColor,
+              value: _fuelValue,
+              onChanged: (newValue) {
+                setState(() {
+                  _fuelValue = newValue;
+                });
+              },
+              divisions: 100,
+              min: 0,
+              max: 20000,
+            ),
+          ),
+          SizedBox(
+            height: size.height * 1 / 100,
+          ),
+          Row(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(left: size.width * 5 / 100),
+                child: Text(
+                  "Groceries",
+                  style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    textStyle: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+              ),
+              Spacer(),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(right: size.width * 5 / 100),
+                child: Text(
+                  "₹ " + _groceriesValue.round().toString(),
+                  style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    textStyle: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: size.width * 5 / 100, right: size.width * 5 / 100),
+            child: Slider(
+              activeColor: Theme.of(context).accentColor,
+              inactiveColor: Theme.of(context).backgroundColor,
+              value: _groceriesValue,
+              onChanged: (newValue) {
+                setState(() {
+                  _groceriesValue = newValue;
+                });
+              },
+              divisions: 100,
+              min: 0,
+              max: 20000,
+            ),
+          ),
+          SizedBox(
+            height: size.height * 1 / 100,
+          ),
+          Row(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(left: size.width * 5 / 100),
+                child: Text(
+                  "Miscellaneous",
+                  style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    textStyle: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+              ),
+              Spacer(),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(right: size.width * 5 / 100),
+                child: Text(
+                  "₹ " + _miscellaneousValue.round().toString(),
+                  style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    textStyle: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: size.width * 5 / 100, right: size.width * 5 / 100),
+            child: Slider(
+              activeColor: Theme.of(context).accentColor,
+              inactiveColor: Theme.of(context).backgroundColor,
+              value: _miscellaneousValue,
+              onChanged: (newValue) {
+                setState(() {
+                  _miscellaneousValue = newValue;
+                });
+              },
+              divisions: 100,
+              min: 0,
+              max: 20000,
+            ),
+          ),
+          SizedBox(
+            height: size.height * 5 / 100,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Theme.of(context).accentColor,
+            ),
+            alignment: Alignment.center,
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: size.width * 25 / 100),
+            child: FlatButton(
+              onPressed: () {},
+              child: Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: size.height * 2 / 100),
+                child: Text(
+                  "Continue",
+                  style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    textStyle: Theme.of(context).textTheme.headline2,
+                  ),
+                ),
+              ),
             ),
           ),
         ],
